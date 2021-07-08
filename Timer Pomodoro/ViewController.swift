33bddcef1857f6d9f30f50a2b469527276eb5d62
Anastasiia Maksimova <sia.m94@ya.ru>
2021-07-08 09:20:57 +0500

    Кнопка и фон.

--- Timer Pomodoro/ViewController.swift
+++ Timer Pomodoro/ViewController.swift
@@ -9,11 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private lazy var startButton: UIButton = {
+        let button = UIButton()
+        button.layer.cornerRadius = 10
+        button.setTitle("Начать", for: .normal)
+        button.backgroundColor = .black
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    
+    // MARK: - lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupVew()
+        setupLayout()
     }
 
 
+    // MARK: - Settings
+    
+    private func setupLayout() {
+        view.addSubview(startButton)
+        NSLayoutConstraint.activate([
+            startButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -100),
+            startButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+        startButton.heightAnchor.constraint(equalToConstant: 50),
+        startButton.widthAnchor.constraint(equalToConstant: 150)
+        ])
+    }
+    
+    private func setupVew() {
+        view.backgroundColor = .systemYellow
+    }
 }
 
